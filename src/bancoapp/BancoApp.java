437285434fbb6c0d60999582a92b0e4e489163a5
Gustavo <guscalure43@gmail.com>
2022-04-30 21:02:27 -0600

    Creacion paquete de logica de negocios

--- src/bancoapp/BancoApp.java
+++ src/bancoapp/BancoApp.java
@@ -1,5 +1,7 @@
 package bancoapp;
 
+import serviciosexternos.TipoCambio;
+
 /**
  *
  * @author Gustavo
@@ -10,6 +12,8 @@ public class BancoApp {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
+		TipoCambio tc = new TipoCambio();
+		System.out.println(tc.convertirAColones(100));
 	}
 	
 }
