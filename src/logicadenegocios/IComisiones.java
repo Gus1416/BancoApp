330b27e4cb0524918ed60f369f2f5ede9eae39f7
97ale <92416062+97ale@users.noreply.github.com>
2022-04-30 21:58:57 -0600

    Creacion Interfaz IComisiones

---
+++ src/logicadenegocios/IComisiones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package logicadenegocios;
+
+/**
+ *
+ * @author Alejandra Merino
+ */
+public interface IComisiones {
+  
+  public double calcularTotalComisionesDepositos();
+  public double calcularTotalComisionesRetiros ();
+  public double calcularTotalComisiones();
+}
