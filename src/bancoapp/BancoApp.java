bb2f2a0e8d6affd2c4deb93a566441fba7083960
97ale <92416062+97ale@users.noreply.github.com>
2022-05-09 14:03:35 -0600

    Update BancoApp.java

--- src/bancoapp/BancoApp.java
+++ src/bancoapp/BancoApp.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import logicadeaccesodedatos.ClienteCRUD;
 import logicadenegocios.Cliente;
+import logicadevalidacion.ValidacionPersona;
 
 /**
  *
@@ -15,11 +16,6 @@ public class BancoApp {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		ClienteCRUD clienteCrud = new ClienteCRUD();
-//		clienteCrud.registrarCliente(new Cliente("202220222", "Gonzales", "Figueres", "José", new Date(), "41168545", "chema@gmail.com"));
-		ArrayList<Cliente> clientes = clienteCrud.consultarClientes();
-		for (Cliente cliente : clientes){
-			System.out.println(cliente.toString());
-		}
+          
 	}
 }
